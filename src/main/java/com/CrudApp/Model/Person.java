6a7a501608641a5d2c8fb19eb607349dcefdd81c
Muhammet Keskin <muhammetkeskin72@trakya.edu.tr>
2024-06-14 18:47:41 +0300

    getter and setter added to model class

---
+++ src/main/java/com/CrudApp/Model/Person.java
@@ -0,0 +1,71 @@
+package com.CrudApp.Model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private String gender;
+    private String favouriteTeam;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getFavouriteTeam() {
+        return favouriteTeam;
+    }
+
+    public void setFavouriteTeam(String favouriteTeam) {
+        this.favouriteTeam = favouriteTeam;
+    }
+
+
+
+
+}
