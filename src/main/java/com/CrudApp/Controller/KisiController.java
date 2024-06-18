1e5219f38d83a510af8b5746a3a4c5395fc325c7
Muhammet Keskin <muhammetkeskin72@trakya.edu.tr>
2024-06-18 03:42:02 +0300

    Redundant comments between codes were deleted and edited

--- src/main/java/com/CrudApp/Controller/KisiController.java
+++ src/main/java/com/CrudApp/Controller/KisiController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-// @RestController anotasyonu, bu sınıfın bir RESTful web servis denetleyicisi olduğunu belirtir.
+
 @RestController
 @RequestMapping("/api/kisiler")
 @CrossOrigin(origins = "http://localhost:3000")
