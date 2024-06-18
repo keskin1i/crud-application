6a1b1423b40b0038b06cfa023c5b21e56defff9a
Muhammet Keskin <muhammetkeskin72@trakya.edu.tr>
2024-06-18 03:39:37 +0300

    Redundant comments between codes were deleted and edited

--- src/main/java/com/CrudApp/Repository/KisiRepository.java
+++ src/main/java/com/CrudApp/Repository/KisiRepository.java
@@ -4,7 +4,7 @@ package com.CrudApp.Repository;
 import com.CrudApp.Model.Kisi;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// KisiRepository arayüzü, Kisi nesneleri için CRUD işlemlerini sağlar.
+
 public interface KisiRepository extends JpaRepository<Kisi, Long> {
 }
 
