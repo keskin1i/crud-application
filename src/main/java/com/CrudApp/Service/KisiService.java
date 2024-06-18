843480e111d77f927ed87b7fb2a9ab966956c4ac
Muhammet Keskin <muhammetkeskin72@trakya.edu.tr>
2024-06-18 03:40:04 +0300

    Redundant comments between codes were deleted and edited

--- src/main/java/com/CrudApp/Service/KisiService.java
+++ src/main/java/com/CrudApp/Service/KisiService.java
@@ -9,30 +9,30 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-// @Service anotasyonu, bu sınıfın bir servis bileşeni olduğunu belirtir.
+
 @Service
 public class KisiService {
 
-    // KisiRepository'nin otomatik olarak enjekte edilmesini sağlar.
+
     @Autowired
     private KisiRepository kisiRepository;
 
-    // Tüm kişileri getirir.
+
     public List<Kisi> findAll() {
         return kisiRepository.findAll();
     }
 
-    // Belirli bir ID'ye sahip kişiyi getirir.
+
     public Optional<Kisi> findById(Long id) {
         return kisiRepository.findById(id);
     }
 
-    // Yeni bir kişi kaydeder veya mevcut bir kişiyi günceller.
+
     public Kisi save(Kisi kisi) {
         return kisiRepository.save(kisi);
     }
 
-    // Belirli bir ID'ye sahip kişiyi siler.
+
     public void deleteById(Long id) {
         kisiRepository.deleteById(id);
     }
