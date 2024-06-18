6bfb16be3bc25fb2ce573b4c8386360f900dd3e1
Muhammet Keskin <muhammetkeskin72@trakya.edu.tr>
2024-06-18 03:36:42 +0300

    redundant pages deleted

--- src/main/java/com/CrudApp/Repository/Repository.java
+++
@@ -1,8 +0,0 @@
-/*package com.CrudApp.Repository;
-
-import com.CrudApp.Model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Repository extends JpaRepository<Person, Long> {
-
-}*/
